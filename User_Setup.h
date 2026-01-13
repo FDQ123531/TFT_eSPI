13007f1456d35b0bfa565d5262cd405c02ede307
FDQ123531 <2068085991@qq.com>
2026-01-13 22:41:25 +0800

    Update User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -210,7 +210,7 @@
 // The hardware SPI can be mapped to any pins
 
 //#define TFT_MISO 19
-//#define TFT_MOSI 23
+//#define TFT_MOSI 8
 //#define TFT_SCLK 18
 //#define TFT_CS   15  // Chip select control pin
 //#define TFT_DC    2  // Data Command control pin
